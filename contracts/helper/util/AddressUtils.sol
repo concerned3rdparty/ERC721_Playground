0e611416f1e9da99c9184a916b21e4d994f836a4
ismail <ismailkelebek1@gmail.com>
2018-11-17 23:04:32 +0300

    added addressutls library with a method to checkt an address is a contract or not

---
+++ contracts/helper/util/AddressUtils.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.18;
+
+/**
+* Utility library of inline functions on addresses
+*/
+library AddressUtils {
+
+  /**
+   * @dev This function will return false if invoked during the constructor of a contract,
+   * as the code is not actually created until after the constructor finishes.
+   * @param addr address to check
+   * @return whether the target address is a contract
+   */
+  function isContract(address addr) internal view returns (bool) {
+    uint256 size;
+    // TODO Check this again before the Serenity release, because all addresses will be
+    // contracts then.
+    // solium-disable-next-line security/no-inline-assembly
+    assembly { size := extcodesize(addr) }
+    return size > 0;
+  }
+
+}
